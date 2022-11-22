7d7a4de4ead7fac2239b338b9a7cd20b7b264eaa
Ayush Bobale <bobaleayush@gmail.com>
2022-11-22 23:11:53 +0530

    Minor bug fix

--- src/client/src/components/HamMenu/HamMenu.js
+++ src/client/src/components/HamMenu/HamMenu.js
@@ -2,7 +2,7 @@ import './HamMenu.css'
 
 function HamMenu(props) {
     return(
-        <div className={props.hide ? "side-menu active" : "side-menu"}>
+        <div className={props.hide ? "side-menu side-menu-active" : "side-menu"}>
             <div className="side-menu-item active-link">List items</div>
             <div className="side-menu-item">How renting works</div>
             <div className="side-menu-item">Rentkar for Business</div>
---
+++ src/readme.md
@@ -0,0 +1,18 @@
+# Frontend Design
+
+
+## How to run
+
+
+```bash
+git clone https://github.com/AyushBobale/Figma-Assessment.git
+cd Figma-Assessment/src/client
+npm i
+npm start
+```
+
+## Todo:
+
+1. Slider
+2. category tabs - grid layout
+3. 
